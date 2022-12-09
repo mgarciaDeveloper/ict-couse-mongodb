fb1e6d165ef5853775eafc81df745f60f65d2460
Matheus <matheusgarcia.developer@gmail.com>
2022-12-08 21:36:58 -0300

    registro de usuários configurados para sessão

--- models/Users.js
+++ models/Users.js
@@ -1,30 +1,15 @@
 var mongoose = require('mongoose')
+const { type } = require('os')
+var passportLocalMongoose = require('passport-local-mongoose')
 
 var theSchema = {
     //característica: JS Type (essa característica é um Número ? É uma string ? )
-    cargo: {
-        type: String,
-        required: true,
-        default: 'Atendente',
-        enum: ['Atendente', 'Gerente', 'Caixa', 'Farmacêutico(a)']
+    username: {
+        type: String
     },
-    name: {
-        type: String,
-        required: true,
-    },
-
-
-    pronome: {
-        type: String,
-        required: true,
-        default: 'senhor',
-        enum: ['senhor', 'senhora', 'senhorita', 'outro']
-    },
-    ingresso: Date
-}
-
+};
 const UsersSchema = new mongoose.Schema(theSchema);
-
+UsersSchema.plugin(passportLocalMongoose)
 //vamos criar a collection!!
 const User = mongoose.model('User', UsersSchema) //são dois campos: o nome da collection e o schema da collection
 
